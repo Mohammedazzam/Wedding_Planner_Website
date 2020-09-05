a2887b47cc41d82383b08682a046c3db09dd8efe
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-09-05 20:22:00 +0300

    section slider

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+$(window).on('load',function(){
+
+    //home section slideshow
+
+    let slideIndex = $(".slide.active").index();
+    const slideLen = $(".slide").length
+
+
+    function slideshow(){
+        $(".slide").removeClass("active").eq(slideIndex).addClass("active");
+        if(slideIndex == slideLen-1){
+            slideIndex = 0;
+        }
+        else{
+            slideIndex++;
+        }
+        setTimeout(slideshow,5000)
+    }
+
+    slideshow()
+
+})
